05604a65dd397e873a3ee9a4b01e94c21a7f1230
saiakashneela <saiakashneela@outlook.com>
2023-05-30 20:44:56 +0100

    Stage 1

---
+++ Computer.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace Test
+{
+    public class Computer
+    {
+        public int Id { get; set;}
+        public string? name { get; set;}
+
+        public bool flag { get; set;}
+
+        public Computer()
+        {
+            if (Id>=1)
+            {
+                Id=1001;
+            }
+        }
+    }
+
+    // internal class Program
+    // {
+    //     static void main(String[] args)
+    //     {
+    //         Computer c = new Computer()
+    //         {
+    //             Id=0,
+    //             name="san",
+    //             flag = false
+
+    //         };
+    //         Console.WriteLine(c.flag);
+    //         Console.WriteLine(c.name);
+    //         Console.WriteLine(c.Id);
+
+    //     }
+    // }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Test;
+
+internal class Program
+    {
+        static void Main(String[] args)
+        {
+            Computer c = new Computer()
+            {
+                Id=0,
+                name="san",
+                flag = false
+
+            };
+            Console.WriteLine(c.flag);
+            Console.WriteLine(c.name);
+            Console.WriteLine(c.Id);
+
+        }
+    }
+
+// Console.WriteLine("Hello, World!!!");
+
+// List<string> firstList = new List<string>();
+
+// firstList.Add("Test");
+
+// Console.WriteLine(firstList[0]);
+// IEnumerable<string> Ilist = firstList;
+
+// Console.WriteLine(Ilist.First());
+
